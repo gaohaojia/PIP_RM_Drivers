0192b592ebe29a368100b37370e115bd525cda3a
gaohaojia <2723687563@qq.com>
2024-04-11 23:39:14 +0800

    change drvier

---
+++ serial_driver/rm_serial_driver/include/rm_serial_driver/rm_serial_driver.hpp
@@ -0,0 +1,62 @@
+// Copyright (c) 2022 ChenJun
+// Licensed under the Apache-2.0 License.
+
+#ifndef RM_SERIAL_DRIVER__RM_SERIAL_DRIVER_HPP_
+#define RM_SERIAL_DRIVER__RM_SERIAL_DRIVER_HPP_
+
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/subscription.hpp>
+#include <serial_driver/serial_driver.hpp>
+
+// C++ system
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "serial_interfaces/msg/vision_recv.hpp"
+#include "serial_interfaces/msg/vision_target.hpp"
+#include "serial_interfaces/msg/referee.hpp"
+
+namespace rm_serial_driver
+{
+class RMSerialDriver : public rclcpp::Node
+{
+public:
+  explicit RMSerialDriver(const rclcpp::NodeOptions & options);
+
+  ~RMSerialDriver() override;
+
+private:
+  void getParams();
+
+  void receiveData();
+
+  void sendData(serial_interfaces::msg::VisionTarget::SharedPtr msg);
+
+  void reopenPort();
+
+  // Serial port
+  std::unique_ptr<IoContext> owned_ctx_;
+  std::string device_name_;
+  std::unique_ptr<drivers::serial_driver::SerialPortConfig> device_config_;
+  std::unique_ptr<drivers::serial_driver::SerialDriver> serial_driver_;
+
+  // Param client to set detect_colr
+  using ResultFuturePtr = std::shared_future<std::vector<rcl_interfaces::msg::SetParametersResult>>;
+
+  rclcpp::Subscription<serial_interfaces::msg::VisionTarget>::SharedPtr target_sub_;
+
+  // For debug usage
+  rclcpp::Publisher<serial_interfaces::msg::VisionRecv>::SharedPtr vision_pub_;
+
+  std::thread receive_thread_;
+};
+}  // namespace rm_serial_driver
+
+#endif  // RM_SERIAL_DRIVER__RM_SERIAL_DRIVER_HPP_
